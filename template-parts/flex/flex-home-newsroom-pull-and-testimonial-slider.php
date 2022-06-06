87f8796fb991030b703576d370ecf0396d436a8c
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-06-06 13:43:12 -0400

    better styles

--- template-parts/flex/flex-home-newsroom-pull-and-testimonial-slider.php
+++ template-parts/flex/flex-home-newsroom-pull-and-testimonial-slider.php
@@ -1,17 +1,27 @@
-<div class="container pt-5 pb-5">
+<div class="container-fluid pt-5 pb-5">
 	<div class="row align-items-center">
-  		<div class="col-lg-8">
-			<h4 class="blue-text mb-2"><?php the_sub_field('newsroom_overline'); ?></h4>
-	    	<h1 class="mb-2"><?php the_sub_field('newsroom_title'); ?></h1>
-	        <p>
-	        	<?php if( have_rows('newsroom_button') ): ?>
-				    <?php while( have_rows('newsroom_button') ): the_row(); ?>
-				       <a class="blue-button" href="<?php the_sub_field('button_url'); ?>"><?php the_sub_field('button_text'); ?></a>
-				    <?php endwhile; ?>
-				<?php endif; ?>	
-	        </p>
+  		<div class="col-lg-8 green-gradient-bg newsroom-bg">
+  			<div class="row">
+  				<div class="col-lg-1">
+  				</div>
+  				<div class="col-lg-6">
+					<h4 class="white-text mb-2"><?php the_sub_field('newsroom_overline'); ?></h4>
+			    	<h1 class="white-text mb-2"><?php the_sub_field('newsroom_title'); ?></h1>
+			    </div>
+			    <div class="col-lg-4" style="text-align:right;">
+			        <p>
+			        	<?php if( have_rows('newsroom_button') ): ?>
+						    <?php while( have_rows('newsroom_button') ): the_row(); ?>
+						       <a class="blue-button" href="<?php the_sub_field('button_url'); ?>"><?php the_sub_field('button_text'); ?></a>
+						    <?php endwhile; ?>
+						<?php endif; ?>	
+			        </p>
+			    </div>
+  				<div class="col-lg-1">
+  				</div>
+		    </div>
 	  	</div>
-	  	<div class="col-lg-4">
+	  	<div class="col-lg-4 bg-lightblue testimonial-bg">
 			Testimonials Section
 	  	</div>
 	</div>
--- template-parts/flex/flex-supporting-organizations-slider.php
+++ template-parts/flex/flex-supporting-organizations-slider.php
@@ -1,12 +1,12 @@
 <div class="container-fluid pt-5 pb-5">
 	<div class="row">
   		<div class="col-lg-12">
-	    	<h1 class="mb-2"><?php the_sub_field('title'); ?></h1>
+	    	<h1 class="mb-2" style="text-align: center;"><?php the_sub_field('title'); ?></h1>
 	  	</div>
 	</div>
 	<div class="row">
   		<div class="col-lg-12">
-	    	<h1 class="mb-2">Organizations List Here</h1>
+	    	<p class="mb-2" style="text-align: center;">Organizations List Here</p>
 	  	</div>
 	</div>
 </div>
