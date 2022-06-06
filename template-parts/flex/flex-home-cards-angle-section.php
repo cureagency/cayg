44f332d39f943f47f39424fd68d3cea279c5a280
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-06-06 13:10:57 -0400

    framework section

--- template-parts/flex/flex-home-cards-angle-section.php
+++ template-parts/flex/flex-home-cards-angle-section.php
@@ -1,5 +1,4 @@
 <div class="container-fluid">
-
 	<div class="container">
 		<div class="row mt-5 mb-5">
 			<div class="col-lg-1">
@@ -14,7 +13,7 @@
 		</div>
 	</div>
 
-	<div class="container bg-lightblue border-radius pt-5 pb-5 mb-5 mt-5">
+	<div class="container bg-lightblue border-radius pt-5 pb-5 mt-5 top-z-index">
 		<div class="row">
 			<div class="col-lg-12 center-text">
 				<h4 class="navy-text">External & Prior Learning</h4>
@@ -35,12 +34,18 @@
 			<?php endif; ?>	
 		</div>
 	</div>
+</div>
+
+<div class="angle-top-bg-navy">
+</div>
+
+<div class="container-fluid bg-navy pt-5 pb-5">
 
 	<div class="container">
 		<div class="row mt-5 pt-5">
-			<div class="col-lg-2">
+			<div class="col-lg-1">
 			</div>		
-			<div class="col-lg-6">
+			<div class="col-lg-7">
 				<h4 class="green-text mb-2"><?php the_sub_field('overline'); ?></h4>
 			</div>
 			<div class="col-lg-4">
@@ -48,21 +53,25 @@
 		</div>
 
 
-		<div class="row">
+		<div class="row mt-5">
 			<?php if( have_rows('card_with_image') ): ?>
 				<?php while( have_rows('card_with_image') ): the_row(); ?>
 					<div class="col-lg-4">
-						<a href="<?php the_sub_field('link'); ?>">
-							<h2><?php the_sub_field('title_text'); ?></h2>
-							<p><?php the_sub_field('description_text'); ?></p>
-						</a>
+						<div class="home-research-card">
+							<a href="<?php the_sub_field('link'); ?>">
+								<h2><?php the_sub_field('title_text'); ?></h2>
+								<p><?php the_sub_field('description_text'); ?></p>
+							</a>
+						</div>
+						<div class="home-research-card-image background-image" style="background-image: url('<?php the_sub_field('image'); ?>');">
+						</div>
 					</div>
 			    <?php endwhile; ?>
 			<?php endif; ?>	
 		</div>
 
 
-		<div class="row mt-5 mb-5>
+		<div class="row mt-5 mb-5">
 			<?php if( have_rows('button') ): ?>
 				<?php while( have_rows('button') ): the_row(); ?>
 					<div class="col">
