9d86bc42c328b112c426f90191386c69cc8498e1
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-06-05 23:06:50 -0400

    slider ection

--- template-parts/flex/flex-home-slider.php
+++ template-parts/flex/flex-home-slider.php
@@ -1,4 +1,4 @@
-<div class="">
+<div style="z-index: 20;">
 	<?php if( have_rows('slide') ): ?>
 		<div class="home-slider">			    
 
@@ -9,24 +9,26 @@
 		        <div class="home-slider-slide background-image" style="display: block; position: relative; background-image: url('<?php the_sub_field('image'); ?>');">
 		        	<div class="container" style="height: 100%;">
 			        	<div class="row" style="height: 100%; display: flex;">
-			        		<div class="col" style="align-self: center;">
-					        	<h4 class="green-text mb-3"><?php the_sub_field('overline'); ?></h4>
-					        	<h1 class="white-text"><?php the_sub_field('header'); ?></h1>
-					            <p class="white-text"><?php the_sub_field('paragraph'); ?></p>
+			        		<div class="col-lg-6" style="align-self: center;">
+					        	<h4 class="green-text mb-2"><?php the_sub_field('overline'); ?></h4>
+					        	<h1 class="white-text mb-2"><?php the_sub_field('header'); ?></h1>
+					            <p class="white-text mb-4"><?php the_sub_field('paragraph'); ?></p>
 					            <p>
 					            	<?php if( have_rows('button_1') ): ?>
 									    <?php while( have_rows('button_1') ): the_row(); ?>
-									       <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+									       <a class="green-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
 									    <?php endwhile; ?>
 									<?php endif; ?>	
 
 									<?php if( have_rows('button_2') ): ?>
 									    <?php while( have_rows('button_2') ): the_row(); ?>
-									       <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+									       <a class="nobg-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
 									    <?php endwhile; ?>
 									<?php endif; ?>	
 
 					            </p>
+					        </div>
+					        <div class="col-lg-6">
 					        </div>
 				        </div>
 				    </div>
@@ -36,6 +38,24 @@
 
 		</div>
 	<?php endif; ?>
+</div>
+
+<div class="container">
+	<div class="row align-items-center bg-white home-slider-overlay">
+		<?php if( have_rows('overlay_button') ): ?>
+			<?php while( have_rows('overlay_button') ): the_row(); ?>
+					<div class="col-lg-4">
+						<a href="<?php the_sub_field('link'); ?>">
+							<div class="home-slider-overlay-portion pt-2 pb-2">
+								<h2><?php the_sub_field('title_text'); ?></h2>
+								<p><?php the_sub_field('description_text'); ?></p>
+						    </div>
+						</a>
+					</div>
+				</div>
+		    <?php endwhile; ?>
+		<?php endif; ?>	
+	</div>
 </div>
 
 
