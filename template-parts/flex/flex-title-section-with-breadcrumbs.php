2c4c3902cb8a2df40dd5c9f92fafa4ca33926afa
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-06-06 13:28:20 -0400

    breadcurmbs

--- template-parts/flex/flex-title-section-with-breadcrumbs.php
+++ template-parts/flex/flex-title-section-with-breadcrumbs.php
@@ -7,9 +7,16 @@
 			<div class="col-lg-6">
 			</div>
 		</div>
-		<div class="row">
+		<div class="row mt-3">
 	  		<div class="col-lg-6">
-		    	<p class="white-text">breadcrumbs go here</p>
+		    	<p class="white-text">
+		    		<?php global $post;
+				  if ( $post->post_parent ) { ?>
+				    <a class="nobg-nop-button" href="<?php echo get_permalink( $post->post_parent ); ?>" >
+				    	<?php echo get_the_title( $post->post_parent ); ?>
+				    </a>
+					<?php } ?>
+				</p>
 		  	</div>
 			<div class="col-lg-6">
 			</div>
