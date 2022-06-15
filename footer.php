8c6c11e7f535009f51dec48832df545926387ca1
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-06-15 12:45:49 -0400

    universal join cta

--- footer.php
+++ footer.php
@@ -11,7 +11,27 @@
 
 ?>
 
+<div class="container mt-5 mb-5 pt-5 pb-5 bg-navy border-radius background-image" style="background-image: url('<?php the_field('bg_image', 'option'); ?>');">
+	<div class="row align-items-center">
+		<div class="col-lg-1">
+		</div>
+  		<div class="col-lg-6">
+	    	<h1 class="mb-2 white-text"><?php the_field('cta_title', 'option'); ?></h1>
+	  	</div>
+	  	<div class="col-lg-4" style="text-align: right;">
+	    	<?php if( have_rows('button', 'option') ): ?>
+			    <?php while( have_rows('button', 'option') ): the_row(); ?>
+			       <a class="lightblue-button" href="<?php the_sub_field('url', 'option'); ?>"><?php the_sub_field('text', 'option'); ?></a>
+			    <?php endwhile; ?>
+			<?php endif; ?>	
+	  	</div>
+		<div class="col-lg-1">
+		</div>
+	</div>
+</div>
+
 	<footer id="colophon" class="site-footer">
+
 		<div class="site-info">
 			<div class="container">
 				
