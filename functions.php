44113c6b8c672424091d07c46df24b6c90630f3e
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-07-21 21:01:40 -0400

    blog is now default posts

--- functions.php
+++ functions.php
@@ -627,61 +627,6 @@ add_action( 'init', 'newsletter_init' );
 
 
 /**
- * Register a custom post type called "Blog".
- *
- * @see get_post_type_labels() for label keys.
- */
-function blog_init() {
-    $labels = array(
-        'name'                  => _x( 'Blog', 'Post type general name', 'textdomain' ),
-        'singular_name'         => _x( 'Blog', 'Post type singular name', 'textdomain' ),
-        'menu_name'             => _x( 'Blog', 'Admin Menu text', 'textdomain' ),
-        'name_admin_bar'        => _x( 'Blog', 'Add New on Toolbar', 'textdomain' ),
-        'add_new'               => __( 'Add New', 'textdomain' ),
-        'add_new_item'          => __( 'Add New Blog', 'textdomain' ),
-        'new_item'              => __( 'New Blog', 'textdomain' ),
-        'edit_item'             => __( 'Edit Blog', 'textdomain' ),
-        'view_item'             => __( 'View Blog', 'textdomain' ),
-        'all_items'             => __( 'All Blog Posts', 'textdomain' ),
-        'search_items'          => __( 'Search Blog', 'textdomain' ),
-        'parent_item_colon'     => __( 'Parent Blog:', 'textdomain' ),
-        'not_found'             => __( 'No Blog Posts found.', 'textdomain' ),
-        'not_found_in_trash'    => __( 'No Blog Posts found in Trash.', 'textdomain' ),
-        'featured_image'        => _x( 'Blog Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'set_featured_image'    => _x( 'Set Blog image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'remove_featured_image' => _x( 'Remove Blog image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'use_featured_image'    => _x( 'Use as Blog image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'archives'              => _x( 'Blog archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-        'insert_into_item'      => _x( 'Insert into Blog', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-        'uploaded_to_this_item' => _x( 'Uploaded to this Blog', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-        'filter_items_list'     => _x( 'Filter Blog list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-        'items_list_navigation' => _x( 'Blog list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-        'items_list'            => _x( 'Blog list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-    );
- 
-    $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'blog' ),
-        'capability_type'    => 'post',
-        'menu_icon'    => 'dashicons-edit',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'taxonomies' => array('category', 'post_tag'),
-        'supports'           => array( 'title', 'thumbnail' ),
-    );
- 
-    register_post_type( 'blog', $args );
-}
-
-add_action( 'init', 'blog_init' );
-
-/**
  * Register a custom post type called "In The News".
  *
  * @see get_post_type_labels() for label keys.
