ac25ccd2c75c53eb9d320f7db70e5e5b585d1ef5
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-07-22 08:59:03 -0400

    1 col wysiwyg update

--- template-parts/flex/flex-1-column-wysiwyg.php
+++ template-parts/flex/flex-1-column-wysiwyg.php
@@ -1,11 +1,37 @@
-<div class="container mt-5 mb-5">
-	<div class="row">
-		<div class="col-lg-1">
+<div class="container <?php the_sub_field('top_margin'); ?>-top-margin <?php the_sub_field('bottom_margin'); ?>-bottom-margin" id="<?php the_sub_field('anchor_link'); ?>">
+
+	<?php if( get_sub_field('overline') ): ?>
+		<div class="row align-items-center">
+	  		<div class="col-1">
+		    	<p class="green-text">
+		    		<i class="fa-solid fa-dash"></i>
+		    	</p>
+		  	</div>
+	  		<div class="col-11">
+		    	<h4 class="green-text"><?php the_sub_field('overline'); ?></h4>
+		  	</div>
 		</div>
-  		<div class="col-lg-10">
+	<?php endif; ?>
+
+	<?php if( get_sub_field('title') ): ?>
+		<div class="row small-bottom-margin">
+	  		<div class="col-1">
+		  	</div>
+	  		<div class="col-8">
+		    	<h1><?php the_sub_field('title'); ?></h1>
+		  	</div>
+		  	<div class="col-3">
+		  	</div>
+		</div>
+	<?php endif; ?>
+
+	<div class="row">
+		<div class="col-1">
+		</div>
+  		<div class="col-10">
 	    	<?php the_sub_field('content'); ?>
 	  	</div>
-		<div class="col-lg-1">
+		<div class="col-1">
 		</div>
 	</div>
 </div>
