554842591a966c6e07d0a2b99cafff8962aee336
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-07-22 10:05:18 -0400

    small header

--- template-parts/flex/flex-title-section-with-breadcrumbs.php
+++ template-parts/flex/flex-title-section-with-breadcrumbs.php
@@ -1,4 +1,4 @@
-<div class="container-fluid large-topandbottom-padding bg-navy background-image" 
+<div class="container-fluid small-topandbottom-padding bg-navy background-image" 
 	<?php 
 	$image = get_sub_field('background_image');
 	if( !empty( $image ) ): ?> 
