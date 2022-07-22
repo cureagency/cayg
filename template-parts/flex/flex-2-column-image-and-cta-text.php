8d5d561da74b4203d77abf5166b1344dfb7df717
Peyton Sorah <peytonsorah@peytons-mbp-2.lan>
2022-07-22 09:07:01 -0400

    2 col edits

--- template-parts/flex/flex-2-column-image-and-cta-text.php
+++ template-parts/flex/flex-2-column-image-and-cta-text.php
@@ -1,75 +1,78 @@
-<?php if( get_sub_field('orientation') == 'img-left' ) { ?>
+<div class="container <?php the_sub_field('top_margin'); ?>-top-margin <?php the_sub_field('bottom_margin'); ?>-bottom-margin" id="<?php the_sub_field('anchor_link'); ?>">
 
-<div class="container mt-5 mb-5">
 	<div class="row align-items-center">
-  		<div class="col-lg-5">
-  			<?php if( get_sub_field('image') ): ?>
-			    <img class="border-radius" src="<?php the_sub_field('image'); ?>" />
-			<?php endif; ?>
-	  	</div>
-	  	<div class="col-lg-1">
-	  	</div>
-	  	<div class="col-lg-5">
-			<h4 class="blue-text mb-2"><?php the_sub_field('overline'); ?></h4>
-	    	<h1 class="mb-2"><?php the_sub_field('title'); ?></h1>
-	        <p class="mb-4"><?php the_sub_field('paragraph'); ?></p>
-	        <p>
-	        	<?php if( have_rows('button_1') ): ?>
-				    <?php while( have_rows('button_1') ): the_row(); ?>
-				       <a class="blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
-				    <?php endwhile; ?>
-				<?php endif; ?>	
 
-				<?php if( have_rows('button_2') ): ?>
-				    <?php while( have_rows('button_2') ): the_row(); ?>
-				       <a class="nobg-blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
-				    <?php endwhile; ?>
-				<?php endif; ?>	
+		<?php if( get_sub_field('orientation') == 'img-left' ) { ?>
 
-	        </p>
-	  	</div>
-	    <div class="col-lg-1">
-	  	</div>
+		  		<div class="col-lg-5">
+		  			<?php if( get_sub_field('image') ): ?>
+					    <img class="border-radius" src="<?php the_sub_field('image'); ?>" />
+					<?php endif; ?>
+			  	</div>
+			  	<div class="col-lg-1">
+			  	</div>
+			  	<div class="col-lg-5">
+					<h4 class="blue-text mb-2"><?php the_sub_field('overline'); ?></h4>
+			    	<h1 class="mb-2"><?php the_sub_field('title'); ?></h1>
+			        <p class="mb-4"><?php the_sub_field('paragraph'); ?></p>
+			        <p>
+			        	<?php if( have_rows('button_1') ): ?>
+						    <?php while( have_rows('button_1') ): the_row(); ?>
+						    	<?php if( get_sub_field('url') ): ?>
+							       <a class="blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+							    <?php endif; ?>	
+						    <?php endwhile; ?>
+						<?php endif; ?>	
+
+						<?php if( have_rows('button_2') ): ?>
+						    <?php while( have_rows('button_2') ): the_row(); ?>
+						    	<?php if( get_sub_field('url') ): ?>
+							       <a class="nobg-blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+							     <?php endif; ?>	
+						    <?php endwhile; ?>
+						<?php endif; ?>	
+
+			        </p>
+			  	</div>
+			    <div class="col-lg-1">
+			  	</div>
+
+
+		<?php } ?>
+
+		<?php if( get_sub_field('orientation') == 'img-right' ) { ?>
+
+			    <div class="col-lg-1">
+			  	</div>
+			  	<div class="col-lg-5">
+					<h4 class="blue-text mb-2"><?php the_sub_field('overline'); ?></h4>
+			    	<h1 class="mb-2"><?php the_sub_field('title'); ?></h1>
+			        <p class="mb-4"><?php the_sub_field('paragraph'); ?></p>
+			        <p>
+			        	<?php if( have_rows('button_1') ): ?>
+						    <?php while( have_rows('button_1') ): the_row(); ?>
+						       <a class="blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+						    <?php endwhile; ?>
+						<?php endif; ?>	
+
+						<?php if( have_rows('button_2') ): ?>
+						    <?php while( have_rows('button_2') ): the_row(); ?>
+						       <a class="nobg-blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
+						    <?php endwhile; ?>
+						<?php endif; ?>	
+
+			        </p>
+			  	</div>
+			    <div class="col-lg-1">
+			  	</div>
+			  	<div class="col-lg-5">
+		  			<?php if( get_sub_field('image') ): ?>
+					    <img class="border-radius" src="<?php the_sub_field('image'); ?>" />
+					<?php endif; ?>
+			  	</div>
+
+		<?php } ?>
 	</div>
 </div>
-
-<?php } ?>
-
-<?php if( get_sub_field('orientation') == 'img-right' ) { ?>
-
-<div class="container mt-5 mb-5">
-	<div class="row align-items-center">
-	    <div class="col-lg-1">
-	  	</div>
-	  	<div class="col-lg-5">
-			<h4 class="blue-text mb-2"><?php the_sub_field('overline'); ?></h4>
-	    	<h1 class="mb-2"><?php the_sub_field('title'); ?></h1>
-	        <p class="mb-4"><?php the_sub_field('paragraph'); ?></p>
-	        <p>
-	        	<?php if( have_rows('button_1') ): ?>
-				    <?php while( have_rows('button_1') ): the_row(); ?>
-				       <a class="blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
-				    <?php endwhile; ?>
-				<?php endif; ?>	
-
-				<?php if( have_rows('button_2') ): ?>
-				    <?php while( have_rows('button_2') ): the_row(); ?>
-				       <a class="nobg-blue-button" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('text'); ?></a>
-				    <?php endwhile; ?>
-				<?php endif; ?>	
-
-	        </p>
-	  	</div>
-	    <div class="col-lg-1">
-	  	</div>
-	  	<div class="col-lg-5">
-  			<?php if( get_sub_field('image') ): ?>
-			    <img class="border-radius" src="<?php the_sub_field('image'); ?>" />
-			<?php endif; ?>
-	  	</div>
-	</div>
-</div>
-
-<?php } ?>
 
 
